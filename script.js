ef4c81e627b837525b49275e922d37aa441fc480
Shubham Bharti <shubhambharti409sb5019175@gmail.com>
2025-10-27 23:17:18 +0530

    Initail Commit

---
+++ script.js
@@ -0,0 +1,100 @@
+function maskPassword(pass){
+    let str=""
+    for (let index = 0; index < pass.length; index++) {
+        str += "*"
+    }
+    return str
+}
+
+const deletePasswords = (website) => {
+    let data = localStorage.getItem("passwords")
+    let arr = JSON.parse(data);
+    arrUpdated= arr.filter((e) => {
+        return e.website != website
+    })
+    localStorage.setItem("passwords", JSON.stringify(arrUpdated))
+    alert('Successfully deleted password')
+    showPasswords()
+
+
+}
+
+const copyText = (text) => {
+    // Function to copy text to the clipboard
+    const tempInput = document.createElement("textarea");
+    tempInput.value = text;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+    // alert('Text copied to clipboard');
+}
+
+
+const showPasswords = () => {
+    //logic to fill the table
+    let tb = document.querySelector("table")
+    let data = localStorage.getItem("passwords")
+    if (data == null || JSON.parse(data).length == 0) {
+        tb.innerHTML = "No Data to show"
+
+    }
+    else {
+        tb.innerHTML = `<tr>
+        <th>Wesites</th>
+        <th>Username</th>
+        <th>Password</th>
+        <th>Delete</th>
+    </tr>`
+    
+        let arr = JSON.parse(data);
+        let str = ""
+        for (let index = 0; index < arr.length; index++) {
+            const element = arr[index];
+
+            str += `<tr>
+        <td>${element.website} <img onclick="copyText('${element.website}')" src="copy.svg" alt="Copy" width="50" align="right" height="32"></td>
+        <td>${element.username} <img onclick="copyText('${element.username}')" src="copy.svg" alt="Copy" width="50" align="right" height="32"></td>
+        <td>${maskPassword(element.password)} <img onclick="copyText('${element.password}')" src="copy.svg" alt="Copy" width="50" align="right"  height="32"></td>
+        <td><button class="btnsm" onclick="deletePasswords('${element.website}')">Delete</button></td>
+    </tr>`;
+        }
+
+        tb.innerHTML = tb.innerHTML + str
+    }
+
+    website.value = ""
+    username.value = ""
+    password.value = ""
+}
+
+console.log("working");
+showPasswords()
+document.addEventListener('DOMContentLoaded', function () {
+    const username = document.getElementById("username");
+    const password = document.getElementById("password");
+
+    document.querySelector(".btn").addEventListener("click", (e) => {
+        e.preventDefault();
+        console.log("Clicked.....");
+        console.log(username.value, password.value);
+        let passwords = localStorage.getItem("passwords")
+        console.log(passwords)
+
+        if (passwords == null) {
+            let json = []
+            json.push({ website:website.value, username: username.value, password: password.value })
+            alert("Password Saved")
+            localStorage.setItem("passwords", JSON.stringify(json))
+
+        }
+        else {
+            let json = JSON.parse(localStorage.getItem("passwords"))
+            json.push({ website:website.value, username: username.value, password: password.value })
+            alert("Password Saved")
+            localStorage.setItem("passwords", JSON.stringify(json))
+
+        }
+        showPasswords()
+    });
+});
